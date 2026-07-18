c6ca57d300612631c60a142dbc3714bd4f3dfbea
Narayan Zade <narayan.d.zade@gmail.com>
2026-07-18 15:40:45 +0530

    newsign updated

---
+++ takto.php
@@ -0,0 +1,28 @@
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+  var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+
+  Tawk_API.onLoad = function() {
+    if (window.innerWidth <= 768) {
+      Tawk_API.hideWidget();
+    }
+  };
+
+  window.addEventListener('resize', function() {
+    if (window.innerWidth <= 768) {
+      Tawk_API.hideWidget();
+    } else {
+      Tawk_API.showWidget();
+    }
+  });
+
+  (function(){
+    var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+    s1.async = true;
+    s1.src = 'https://embed.tawk.to/6a2b77358705f01c3509ad02/1jqssjaou';
+    s1.charset = 'UTF-8';
+    s1.setAttribute('crossorigin', '*');
+    s0.parentNode.insertBefore(s1, s0);
+  })();
+</script>
+<!--End of Tawk.to Script-->
